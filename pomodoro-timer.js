8e5db5c1be7c581487c5050b6e72dca5eab854c9
Alister N. Mada <alistermada@gmail.com>
2015-11-22 20:16:42 +0700

    added nextSession function

--- pomodoro-timer.js
+++ pomodoro-timer.js
@@ -1,23 +1,65 @@
 angular
   .module('pomodoro-timer', [])
   .controller('TimerController', ['$scope', '$interval', function($scope, $interval) {
+    $scope.timerRunning = false;
     $scope.pomodoroLength = 25;
     $scope.shortBreakLength = 5;
     $scope.longBreakLength = 20;
     $scope.session = "pomodoro";
-    $scope.$watchGroup(['pomodoroLength', 'shortBreakLength', 'longBreakLength'], function() {
-      $scope.time = $scope[$scope.session + "Length"] * 60 * 1000;
+    $scope.pomodorosPerSet = 4;
+    $scope.pomodoroNumber = 1;
+
+    $scope.$watch('time', function() {
+      if ($scope.time <= 0) {
+        $scope.nextSession();
+      }
     });
-    var timerRunning = false;
-    $scope.toggleTimer = function() {
-      if (timerRunning) {
-        $interval.cancel($scope.timer);
-        timerRunning = false;
-      } else {
+
+    $scope.$watchGroup(['pomodoroLength', 'shortBreakLength', 'longBreakLength'], function(newValue) {
+      resetTime();
+    });
+
+    $scope.validate = function(input) {
+      console.log(input)
+    }
+
+    $scope.startTimer = function() {
+      console.log($scope.value);
+      if (!$scope.timerRunning) {
         $scope.timer = $interval(function(){
           $scope.time -= 1000;
         }, 1000);
-        timerRunning = true;
+        $scope.timerRunning = true;
       }
+    }
+
+    $scope.stopTimer = function(reset) {
+      if ($scope.timerRunning) {
+        $interval.cancel($scope.timer);
+        $scope.timerRunning = false;
+      }
+      if (reset) {
+        resetTime(); 
+      }
+    }
+
+    $scope.nextSession = function() {
+      if ($scope.session === "pomodoro") {
+        if ($scope.pomodoroNumber === $scope.pomodorosPerSet) {
+          $scope.session = "longBreak";
+          $scope.pomodoroNumber = 0;
+        } else {
+          $scope.session = "shortBreak";
+        }
+      } else {
+        $scope.session = "pomodoro";
+        $scope.pomodoroNumber += 1;
+      }
+      
+      resetTime();
+    }
+
+    function resetTime() {
+      $scope.time = $scope[$scope.session + "Length"] * 60 * 1000;
     }
   }]);
