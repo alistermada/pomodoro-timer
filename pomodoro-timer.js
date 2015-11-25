1225b8dff1dccc16091c264706f49a8fcb2ac683
Alister N. Mada <alistermada@gmail.com>
2015-11-25 20:49:59 +0700

    input display fixes and sound

--- pomodoro-timer.js
+++ pomodoro-timer.js
@@ -5,9 +5,11 @@ angular
     $scope.pomodoro = 25;
     $scope.shortBreak = 5;
     $scope.longBreak = 20;
-    $scope.session = "pomodoro";
+    $scope.currentSession = 'pomodoro';
     $scope.pomodorosPerSet = 4;
     $scope.pomodoroNumber = 1;
+    $scope.time = $scope[$scope.currentSession] * 60000;
+    var sessions = ['pomodoro', 'shortBreak', 'longBreak'];
 
     $scope.$watch('time', function() {
       if ($scope.time <= 0) {
@@ -15,12 +17,19 @@ angular
       }
     });
 
-    $scope.$watchGroup(['pomodoro', 'shortBreak', 'longBreak'], function(newValue) {
-      resetTime();
+    $scope.$watchGroup(sessions, function(newValue, oldValue) {
+      for (var i = 0; i < sessions.length; i++) {
+        if (newValue[i] !== oldValue[i] && sessions[i] === $scope.currentSession) {
+          updateTime();
+        }
+      }
     });
 
+    $scope.highlight = function($event) {
+      $event.currentTarget.select();
+    }
+
     $scope.startTimer = function() {
-      console.log($scope.value);
       if (!$scope.timerRunning) {
         $scope.timer = $interval(function(){
           $scope.time -= 1000;
@@ -40,22 +49,33 @@ angular
     }
 
     $scope.nextSession = function() {
-      if ($scope.session === "pomodoro") {
+      if ($scope.currentSession === "pomodoro") {
         if ($scope.pomodoroNumber === $scope.pomodorosPerSet) {
-          $scope.session = "longBreak";
+          $scope.currentSession = "longBreak";
           $scope.pomodoroNumber = 0;
         } else {
-          $scope.session = "shortBreak";
+          $scope.currentSession = "shortBreak";
         }
       } else {
-        $scope.session = "pomodoro";
+        $scope.currentSession = "pomodoro";
         $scope.pomodoroNumber += 1;
       }
       
       resetTime();
+      playTimerBeep();
+    }
+
+    function playTimerBeep() {
+      document.getElementById("timerBeep").play();
     }
 
     function resetTime() {
-      $scope.time = $scope[$scope.session] * 60 * 1000;
+      $scope.time = $scope[$scope.currentSession] * 60000;
+    }
+
+    function updateTime() {
+      var remainingSeconds = $scope.time % 60000;
+      resetTime();
+      $scope.time += remainingSeconds;
     }
   }]);
